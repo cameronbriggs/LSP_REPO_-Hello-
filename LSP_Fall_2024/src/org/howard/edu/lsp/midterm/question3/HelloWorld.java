1e20af31adeae6d230aa2d8bd8c505ea82a05565
cameronbriggs <98107637+cameronbriggs@users.noreply.github.com>
2024-10-15 17:47:00 -0400

    Create HelloWorld.java

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question3/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Name: Cameron Briggs
+ */
+
+package org.howard.edu.lsp.midterm.question3;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
