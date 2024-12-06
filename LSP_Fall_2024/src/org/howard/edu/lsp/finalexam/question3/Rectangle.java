82bede473c5021dc2feffb2cb489340d45328b25
cameronbriggs <98107637+cameronbriggs@users.noreply.github.com>
2024-12-05 19:33:30 -0500

    Create Rectangle.java

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question3/Rectangle.java
@@ -0,0 +1,9 @@
+/**
+ * Implementation of a Rectangle shape.
+ */
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
